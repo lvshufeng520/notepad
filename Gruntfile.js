670ddf80caa3ea833955caa4f907ce35427466d3
lvshufeng520 <1002463595@qq.com>
2020-04-09 01:29:31 +0800

    min

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: 'dist/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    copy: {
+      html: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    concat: {
+      js: {
+        src: ['js/*.js'],
+        dest: 'dist/bundle.js'
+      },
+      css: {
+        src: ['css/*.css'],
+        dest: 'dist/bundle.css'
+      }
+    },
+    uglify: {
+      'dist/bundle.min.js': 'dist/bundle.js'
+    },
+    cssmin: {
+      'dist/bundle.min.css': 'dist/bundle.css'
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/index.html']
+    },
+    clean: {
+      end: ['dist/bundle.css', 'dist/bundle.js', '.tmp']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('build', ['copy:html', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'htmlmin', 'clean:end']);
+};
