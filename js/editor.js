3f7d055cf41c9c6e0ed489b2584a13f048b5342c
lvshufeng520 <1002463595@qq.com>
2020-04-09 00:10:00 +0800

    new

--- js/editor.js
+++
@@ -1,8 +0,0 @@
-var editor = (function() {
-    var $box = $('#box');
-    var ed = 
-        '<div class="notepad-editor" style="bottom: 0px;">' +
-        '<textarea spellcheck="false" auto-size="none" wrap="soft" style="overflow-x: hidden; font-family: Arial; font-size: 16pt;"></textarea>' +
-        '</div>';
-    $box.append(ed);
-})
